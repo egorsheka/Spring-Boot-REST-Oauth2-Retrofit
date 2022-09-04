0a1323391231f8fa4fe8eb3cfb2c72b88babe4a4
Egor <y.sheka@sam-solutions.com>
2022-09-04 19:17:35 +0300

    init project, add main dependencies

---
+++ src/test/java/com/sandbox/sheka/Oauth2ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.sandbox.sheka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2ApplicationTests
+{
+
+	@Test
+	void contextLoads() {
+	}
+
+}
